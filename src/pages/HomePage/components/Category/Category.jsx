38dc5cfba321e2c6ebcdeb630e0f3e5ea64f8263
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2023-01-11 22:04:02 +0500

    initial commit

---
+++ src/pages/HomePage/components/Category/Category.jsx
@@ -0,0 +1,131 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Category.css";
+import machine from "../../assets/img/machine.png";
+import HorizontalScroll from "react-scroll-horizontal";
+import sement from "./sement.png";
+import scroll from "../../assets/img/scroll.jpg";
+
+const Category = () => {
+  return (
+    <div className="category">
+      <div className="container">
+        <HorizontalScroll
+          style={{ overflow: "none", paddingBottom: "210px" }}
+          reverseScroll={true}
+        >
+          <div className="category-page">
+            <div className="category-left">
+              <h2 className="category-name">
+                ARIF international holding jamoasi loyihalari{" "}
+              </h2>
+              <p className="category-text">
+                Hozirgi kunda 6 ta loyihamiz mavhud
+              </p>
+              <Link
+                onClick={() => window.scrollTo({ top: 0 })}
+                className="category-link"
+                to="/"
+              >
+                Батафсил били
+              </Link>
+            </div>
+            <div className="mains">
+              <div className="category-right">
+                <img src={machine} alt="" className="category-img" />
+                <h3 className="category-subname">Sement MCHJ</h3>
+                <p className="category-subtext">
+                  Хитойдан пластик ускуналар олиб келиш хизмати
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="category-page">
+            <div className="category-left">
+              <h2 className="category-name">
+                ARIF international holding jamoasi loyihalari{" "}
+              </h2>
+              <p className="category-text">
+                Hozirgi kunda 6 ta loyihamiz mavhud
+              </p>
+              <Link
+                onClick={() => window.scrollTo({ top: 0 })}
+                className="category-link"
+                to="/"
+              >
+                Батафсил били
+              </Link>
+            </div>
+            <div className="mains">
+              <div className="category-right">
+                <img src={sement} alt="" className="category-img" />
+                <h3 className="category-subname">Sement MCHJ</h3>
+                <p className="category-subtext">
+                  Хитойдан пластик ускуналар олиб келиш хизмати
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="category-page">
+            <div className="category-left">
+              <h2 className="category-name">
+                ARIF international holding jamoasi loyihalari{" "}
+              </h2>
+              <p className="category-text">
+                Hozirgi kunda 6 ta loyihamiz mavhud
+              </p>
+              <Link
+                onClick={() => window.scrollTo({ top: 0 })}
+                className="category-link"
+                to="/"
+              >
+                Батафсил били
+              </Link>
+            </div>
+            <div className="mains">
+              <div className="category-right">
+                <img src={machine} alt="" className="category-img" />
+                <h3 className="category-subname">Sement MCHJ</h3>
+                <p className="category-subtext">
+                  Хитойдан пластик ускуналар олиб келиш хизмати
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="category-page">
+            <div className="category-left">
+              <h2 className="category-name">
+                ARIF international holding jamoasi loyihalari{" "}
+              </h2>
+              <p className="category-text">
+                Hozirgi kunda 6 ta loyihamiz mavhud
+              </p>
+              <Link
+                onClick={() => window.scrollTo({ top: 0 })}
+                className="category-link"
+                to="/"
+              >
+                Батафсил били
+              </Link>
+            </div>
+            <div className="mains">
+              <div className="category-right">
+                <img src={sement} alt="" className="category-img" />
+                <h3 className="category-subname">Sement MCHJ</h3>
+                <p className="category-subtext">
+                  Хитойдан пластик ускуналар олиб келиш хизмати
+                </p>
+              </div>
+            </div>
+          </div>
+        </HorizontalScroll>
+        <p className="category-texts">
+          Batafsil bilish uchun scroll qiling{" "}
+          <img src={scroll} alt="" className="category-logo" />{" "}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Category;
---
+++ src/pages/HomePage/components/Header/Header.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import "./Header.css";
+import holcim from "../../assets/img/holcim.png";
+import Modal from "../../pages/Modal/Modal";
+import close from "../../assets/img/+.png";
+import call from "../../assets/img/call.png";
+
+const Header = () => {
+  const [connection, setConnection] = useState(false);
+
+  const handleConnection = () => {
+    setConnection(!connection);
+  };
+
+  return (
+    <div className="header">
+      <div className="container">
+        <h2 className="header-name">Semento цемент ишлаб чикариш корхонаси</h2>
+        <p className="header-text">
+          It doesn’t matter that much to show a really good UI, excellent
+          animated interactions or a complex IA{" "}
+        </p>
+        <div className="header-title">
+          <button className="header-btn">Batafsil</button>
+          <button onClick={handleConnection} className="header-btn">
+            Богланиш
+          </button>
+        </div>
+
+        <div className="header-list">
+          <img src={holcim} alt="" className="header-img" />
+          <p className="header-span">
+            Semento цементлари хакида{" "}
+            <a href="#" className="header-link">
+              батафсил танишиш
+            </a>
+          </p>
+        </div>
+      </div>
+
+      <Modal show={connection}>
+        <button onClick={() => setConnection()} className="header-close">
+          <img src={close} alt="" />
+        </button>
+        <div className="header-modal">
+          <img src={call} alt="" className="header-img" />
+          <div className="header-right">
+            <h3 className="header-subname">Biz bilan bog`laning</h3>
+            <h5 className="header-subnames">O`zimiz aloqaga chiqamiz</h5>
+            <form action="" className="header-form">
+              <input
+                type="name"
+                placeholder="Ism"
+                required
+                className="header-input"
+              />
+              <input
+                type="tell"
+                placeholder="Telefon nomer"
+                required
+                className="header-input"
+              />
+              <button className="header-submit">Yuborish</button>
+            </form>
+          </div>
+        </div>
+      </Modal>
+    </div>
+  );
+};
+
+export default Header;
